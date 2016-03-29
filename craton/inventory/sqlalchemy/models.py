e4f13beed7030fb7d762ac66b9c4f1e353f4067c
Jim Baker <jim.baker@rackspace.com>
2016-03-28 21:43:27 -0600

    Add secrets table and 1-to-many relationship to hosts

--- craton/inventory/sqlalchemy/models.py
+++ craton/inventory/sqlalchemy/models.py
@@ -18,10 +18,12 @@ from sqlalchemy_utils.types.uuid import UUIDType
 import six.moves.urllib.parse as urlparse
 
 
-# FIXME set table_args for mysql?
+# FIXME set up table args for a given database/storage engine, as configured.
+# See https://github.com/rackerlabs/craton/issues/19
 
 
-# From http://docs.sqlalchemy.org/en/latest/_modules/examples/vertical/dictlike.html
+# Implementation is from the example code in
+# http://docs.sqlalchemy.org/en/latest/_modules/examples/vertical/dictlike.html
 # also see related support in HostVariable, Host
 class ProxiedDictMixin(object):
     """Adds obj[key] access to a mapped class.
@@ -66,19 +68,25 @@ Base = declarative_base(cls=CratonBase)
 
 
 class Tenant(Base):
-    """Cloud tenant"""
+    """Supports multitenancy for all other schema elements."""
     __tablename__ = 'tenants'
     id = Column(UUIDType, primary_key=True)
     name = Column(String(255))
-    # FIXME we will surely need to define more columns, but suffices
-    # to define multitenancy for MVP
+    # TODO we will surely need to define more columns, but this
+    # suffices to define multitenancy for MVP
 
-    hosts = relationship("Host", back_populates="tenant")
-    groups = relationship("Group", back_populates="tenant")
+    hosts = relationship('Host', back_populates='tenant')
+    groups = relationship('Group', back_populates='tenant')
 
 
-# Note that for host variables key/value pairs, we assume bytestring, not Unicode
+# FIXME there are stricter requirements for key names in Ansible (see
+# http://docs.ansible.com/ansible/playbooks_variables.html#what-makes-a-valid-variable-name),
+# and it is not clear what the encoding requirements are for values.
+# We may want to represent these requirements with subclassing on
+# HostVariables.
+
 class HostVariables(Base):
+    """Represents specific key/value bindings for a given host."""
     __tablename__ = 'host_variables'
     host_id = Column(ForeignKey('hosts.id'), primary_key=True)
     key = Column(String(255), primary_key=True)
@@ -92,20 +100,24 @@ host_grouping = Table(
     Column('group_id', ForeignKey('groups.id'), primary_key=True))
                       
 
-# FIXME use SqlAlchemy's support for inheritance hierarchies, eg ComputeHost < Host
-# For Sulo's type, compute - what are the actual column types?
-# most likely it makes sense to use single table inheritance;
+# TODO consider using SqlAlchemy's support for inheritance
+# hierarchies, eg ComputeHost < Host but first need to determine what
+# is uniquely required for a ComputeHost; otherwise use an enumerated
+# type to distinguish
+#
 # see http://docs.sqlalchemy.org/en/latest/orm/inheritance.html#single-table-inheritance
 
 class Host(ProxiedDictMixin, Base):
-    """Models descriptive data about a host. Does not model its current state."""
+    """Models descriptive data about a host, including discovered facts"""
     __tablename__ = 'hosts'
     id = Column(UUIDType, primary_key=True)
-    tenant_id = Column(UUIDType, ForeignKey('tenants.id'), index=True, nullable=False)
+    tenant_id = Column(
+        UUIDType, ForeignKey('tenants.id'), index=True, nullable=False)
+    secret_id = Column(UUIDType, ForeignKey('secrets.id'))
     hostname = Column(String(255), nullable=False)
     ip_address = Column(IPAddressType, nullable=False)
-    active = Column(Boolean, default=True)
-    discovered = Column(JSONType)
+    active = Column(Boolean, default=True)  # may or may not be reachable
+    facts = Column(JSONType)  # discovered facts about the host
 
     UniqueConstraint(tenant_id, hostname)
     UniqueConstraint(tenant_id, ip_address)
@@ -118,14 +130,19 @@ class Host(ProxiedDictMixin, Base):
         back_populates='hosts')
 
     # many-to-one relationship with tenants
-    tenant = relationship("Tenant", back_populates="hosts")
+    tenant = relationship('Tenant', back_populates='hosts')
+
+    # optional many-to-one relationship with secrets; don't care about
+    # the backref
+    secret = relationship('Secret')
 
     # provide arbitrary K/V mapping to associated HostVariables table
     variables = relationship(
         'HostVariables',
         collection_class=attribute_mapped_collection('key'))
 
-    # allows access to a host object using dict ops - get/set/del - using [] indexing
+    # allows access to a host object using dict ops - get/set/del -
+    # using [] indexing
     _proxied = association_proxy(
         'variables', 'value',
         creator=lambda key, value: HostVariables(key=key, value=value))
@@ -136,16 +153,29 @@ class Host(ProxiedDictMixin, Base):
 
 
 class Group(Base):
-    """Models a grouping of hosts, which include Ansible groups; OpenStack regions and cells"""
+    """Models a grouping of hosts.
+
+    This includes the following groupings:
+    
+    * Ansible groups
+    * OpenStack regions and cells
+
+    Groups use an adjacency list representation, with possibly some
+    children referring to a given parent.
+    """
     __tablename__ = 'groups'
     id = Column(UUIDType, primary_key=True)
-    tenant_id = Column(UUIDType, ForeignKey('tenants.id'), index=True, nullable=False)
+    tenant_id = Column(
+        UUIDType, ForeignKey('tenants.id'), index=True, nullable=False)
     parent_id = Column(UUIDType, ForeignKey('groups.id'))
     name = Column(String(255))
-    # Our assumption is any config yaml file that needs some sort of include/overlay mechanism
-    # will use Ansible includes/roles; or perhaps something similar for YAML include in general.
-    # So this means we need just one reference.
-    # NOTE we likely need some sort of resolution scheme to handle branches, etc.
+    # Our assumption is any config yaml file that needs some sort of
+    # include/overlay mechanism will use Ansible includes/roles; or
+    # perhaps something similar for YAML include in general.  So this
+    # means we need just one reference.
+    #
+    # NOTE but we likely need some sort of resolution scheme to handle
+    # branches, etc.
     config = Column(URLType)
 
     UniqueConstraint(tenant_id, name)
@@ -154,7 +184,7 @@ class Group(Base):
 
     # self relationship, supporting adjacency list representation
     children = relationship(
-        "Group",
+        'Group',
         backref=backref('parent', remote_side=[id]))
 
     # many-to-many relationship with hosts
@@ -164,4 +194,22 @@ class Group(Base):
         back_populates='groups')
 
     # many-to-one relationship with tenants
-    tenant = relationship("Tenant", back_populates="groups")
+    tenant = relationship('Tenant', back_populates='groups')
+
+
+# TODO we may want to subclass types of secrets
+
+# TODO should determine integration with systems like Barbican that
+# can provide additional secret data to decrypt encrypted certs -
+# which is presumably what we should be storing. See
+# https://github.com/rackerlabs/craton/issues/7
+
+class Secret(Base):
+    """Represents a secret for accessing a host. It may be shared.
+
+    For now we assume a PEM-encoded certificate that represents the
+    private key.
+    """
+    __tablename__ = 'secrets'
+    id = Column(UUIDType, primary_key=True)
+    cert = Column(Text)
