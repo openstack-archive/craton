2fc9e827de8d3420f83a8c92705408b62bcce106
sulochan acharya <sulochan.acharya@rackspace.co.uk>
2016-05-03 10:40:08 +0100

    Add auth middleware for basic auth
    - Adds paste based deploy
    - Adds middleware for both simple and keystone based auth
    - Few restructuring

--- craton/api/README.rst
+++
@@ -1,5 +0,0 @@
-===========================
-Implements REST API support
-===========================
-
-For this support we plan to use Pecan.
--- craton/inventory/api/__init__.py
+++ craton/inventory/api/__init__.py
@@ -1,9 +1,54 @@
+import os
+from paste import deploy
 from flask import Flask
+
+
+from oslo_config import cfg
+from oslo_log import log as logging
 
 from craton.inventory.api import v1
 
 
-def create_app():
+LOG = logging.getLogger(__name__)
+
+api_opts = [
+    cfg.StrOpt('api_paste_config',
+               default="api-paste.ini",
+               help="Configuration file for API service."),
+    cfg.StrOpt('host',
+               default="127.0.0.1",
+               help="API host IP"),
+    cfg.IntOpt('port',
+               default=5000,
+               help="API port to use.")
+]
+
+CONF = cfg.CONF
+opt_group = cfg.OptGroup(name='api',
+                         title='Craton API service group options')
+CONF.register_group(opt_group)
+CONF.register_opts(api_opts, opt_group)
+
+
+def create_app(global_config, **local_config):
+    return setup_app()
+
+
+def setup_app(config=None):
     app = Flask(__name__)
     app.register_blueprint(v1.bp, url_prefix='/v1')
     return app
+
+
+def load_app():
+    cfg_file = None
+    cfg_path = CONF.api.api_paste_config
+    if not os.path.isabs(cfg_path):
+        cfg_file = CONF.find_file(cfg_path)
+    elif os.path.exists(cfg_path):
+        cfg_file = cfg_path
+
+    if not cfg_file:
+        raise cfg.ConfigFilesNotFoundError([cfg.CONF.api.api_paste_config])
+    LOG.info("Loading service with WSGI config: %s" % cfg_file)
+    return deploy.loadapp("config:%s" % cfg_file)
