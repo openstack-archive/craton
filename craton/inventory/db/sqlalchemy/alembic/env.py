292c4aa75d83a06b705ea467f628087b2ecece00
sulochan acharya <sulochan.acharya@rackspace.co.uk>
2016-06-03 09:06:58 +0100

    Adds migrations for database

--- craton/inventory/db/sqlalchemy/alembic/env.py
+++ craton/inventory/db/sqlalchemy/alembic/env.py
@@ -1,6 +1,5 @@
 from __future__ import with_statement
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
 from craton.inventory.db.sqlalchemy import api as sa_api
