92074377eb6c0cde98790b92bf46a8082c89f1fe
Jim Baker <jim.baker@python.org>
2016-06-09 12:15:46 -0600

    Use envvar to enable builds on readthedocs
    oslosphinx is conventionally in test-requirements.txt, but this causes
    issues for RTD builds. Workaround this by the READTHEDOCS envvar for
    such builds.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,15 +16,17 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
+on_read_the_docs = os.environ.get('READTHEDOCS') == 'True'
+
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    #'sphinx.ext.intersphinx',
-    'oslosphinx'
 ]
+if not on_read_the_docs:
+    extensions.append('oslosphinx')
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
