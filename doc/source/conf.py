db32b6f887f43c4909753fe7b086ed6e260a3cee
Michael Turvey <michael.w.turvey@intel.com>
2016-08-31 15:48:01 -0700

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,8 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 on_read_the_docs = os.environ.get('READTHEDOCS') == 'True'
 
+html_style = '/default.css'
+
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
