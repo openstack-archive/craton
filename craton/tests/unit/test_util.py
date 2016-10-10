0b9c151c2e412b18fe2fcdf305ffcff360f71143
Ian Cordasco <graffatcolmingov@gmail.com>
2016-10-10 14:32:38 -0500

    Simplify usage of project_ids in headers and body
    Previously, we were requiring X-Auth-Project headers for authentication
    and a project_id attribute in JSON bodies. This allows for several
    things.
    
    Closes-bug: 1628845
    Closes-bug: 1628917
    Change-Id: I40d0e8b6a3260ff11c493268c8a0553eba62512b

---
+++ craton/tests/unit/test_util.py
@@ -0,0 +1,30 @@
+"""Tests for craton.util module."""
+
+from craton import tests
+from craton import util
+
+
+class TestProjectIdUtilities(tests.TestCase):
+    """Unit tests for the copy_project_id_into_json function."""
+
+    def test_adds_project_id_to_json(self):
+        """Verify we add the project_id to the json body."""
+        self.context.tenant = '1'
+        json = util.copy_project_id_to_json(self.context, {})
+        self.assertDictEqual({'project_id': 1}, json)
+
+    def test_defaults_project_id_to_zero(self):
+        """Verify if there's no tenant attribute on the context we use 0."""
+        del self.context.tenant
+        json = util.copy_project_id_to_json(self.context, {})
+        self.assertDictEqual({'project_id': 0}, json)
+
+    def test_tries_to_convert_projects_to_integers(self):
+        """Verify it raises ValueErrors on non-integer tenants."""
+        try:
+            json = util.copy_project_id_to_json(self.context, {})
+        except ValueError:
+            return
+        else:
+            self.fail("Expected craton.util.copy_project_id_to_json to raise"
+                      " a ValueError but it did not.")
