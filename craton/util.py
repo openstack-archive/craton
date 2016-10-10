05f1fc7953db1ae5ff2ff688105f27f754b35ff8
Ian Cordasco <graffatcolmingov@gmail.com>
2016-10-10 14:32:38 -0500

    Simplify usage of project_ids in headers and body
    Previously, we were requiring X-Auth-Project headers for authentication
    and a project_id attribute in JSON bodies. This allows for several
    things.
    
    Closes-bug: 1628845
    Closes-bug: 1628917
    Change-Id: I40d0e8b6a3260ff11c493268c8a0553eba62512b

---
+++ craton/util.py
@@ -0,0 +1,18 @@
+"""Module containing generic utilies for Craton."""
+
+
+def copy_project_id_into_json(context, json, project_id_key='project_id'):
+    """Copy the project_id from the context into the JSON request body.
+
+    :param context:
+        The request context object.
+    :param json:
+        The parsed JSON request body.
+    :returns:
+        The JSON with the project-id from the headers added as the
+        "project_id" value in the JSON.
+    :rtype:
+        dict
+    """
+    json[project_id_key] = context.tenant
+    return json
