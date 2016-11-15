a4f871360864b8afa1c4bfd9a8280fbd8ec5a55c
Sulochan Acharya <sulochan@gmail.com>
2016-11-15 18:12:27 +0000

    Adds functional testing
    - Adds some structure for functional testing.
    - Partially closes bp for functional tests as not
    all tests are completely written, rather, a structure
    is present that we can iterate upon.
    
    Change-Id: Id1dd11db89e39f93ece56d14cd221083694ded4f

---
+++ craton/tests/functional/test_cell_calls.py
@@ -0,0 +1,21 @@
+from craton.tests.functional import TestCase
+
+
+class APIV1CellTest(TestCase):
+
+    def setUp(self):
+        super(APIV1CellTest, self).setUp()
+        if self.container_setup_error:
+            self.skipTest('Error setting up service container')
+
+    def test_cells_get_all_for_region_1(self):
+        url = self.url + '/v1/cells?region_id=1'
+        cells = self.get(url)
+        self.assertEqual(len(cells.json()), 2)
+        self.assertEqual(['C0001', 'C0002'], [i['name'] for i in cells.json()])
+
+    def test_cells_get_details_for_cell_1(self):
+        url = self.url + '/v1/cells/1'
+        resp = self.get(url)
+        cell = resp.json()
+        self.assertEqual('C0001', cell['name'])
