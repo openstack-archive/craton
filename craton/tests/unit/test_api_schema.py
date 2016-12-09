2cd07667bb8f969922a06eb2e6daaaf937f87593
git-harry <git-harry@live.co.uk>
2016-12-09 11:34:49 +0000

    Ensure route defines endpoint for each schema
    This commit addresses a mismatch between the endpoint names defined in
    routes and those defined in the schemata. Tests have been added to
    ensure that for each schema defined a corresponding route has also been
    defined.
    
    Fixing the mismatch identified two delete calls expecting a 200 status
    code in the response, this has been changed to 204 thereby bringing them
    into line with other delete requests.
    
    Partial-bug: 1644270
    Change-Id: I4cd68a4401d1aaafc6bc3873b206eb54c9f5762b

--- craton/tests/unit/test_api_schema.py
+++ craton/tests/unit/test_api_schema.py
@@ -1,6 +1,7 @@
 import jsonschema
 
-from craton.api.v1.schemas import filters, validators
+from craton import api
+from craton.api.v1.schemas import filters, validators, scopes
 from craton.tests import TestCase
 
 
@@ -30,3 +31,38 @@ def generate_schema_validation_functions(cls):
 
 
 generate_schema_validation_functions(TestAPISchema)
+
+
+class TestSchemaLocationInRoute(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.app = api.setup_app()
+
+
+def generate_endpoint_method_validation_functions(cls):
+    def gen_test(test_type, endpoint, method):
+        def test(self):
+            rules = [
+                rule for rule in self.app.url_map.iter_rules()
+                if rule.endpoint == endpoint and method in rule.methods
+            ]
+            self.assertEqual(len(rules), 1)
+        test_name = 'test_{}_endpoint_method_in_routes_{}_{}'.format(
+            test_type, endpoint, method
+        )
+        setattr(cls, test_name, test)
+
+    for (_endpoint, method) in validators:
+        endpoint = "v1.{}".format(_endpoint)
+        gen_test('validators', endpoint, method)
+
+    for (_endpoint, method) in filters:
+        endpoint = "v1.{}".format(_endpoint)
+        gen_test('filters', endpoint, method)
+
+    for (_endpoint, method) in scopes:
+        endpoint = "v1.{}".format(_endpoint)
+        gen_test('scopes', endpoint, method)
+
+
+generate_endpoint_method_validation_functions(TestSchemaLocationInRoute)
