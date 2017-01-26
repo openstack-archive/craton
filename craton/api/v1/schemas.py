018bc6a925e641c8913fab069d0123701ef0111a
Ian Cordasco <graffatcolmingov@gmail.com>
2017-01-26 13:19:02 -0600

    Add Hypermedia Pagination Links
    This changes all list response bodies to be of the structure defined in
    the specification. For example, it changes responses from /v1/hosts to
    look like:
    
        {
          "hosts": [
            {"id": 1, ...},
            {"id": 2, ...}
          ],
          "links": [
            {
              "rel": "first",
              "href": "http://.../v1/hosts"
            },
            {
              "rel": "prev",
              "href": "http://.../v1/hosts?limit=...&marker=...&..."
            },
            {
              "rel": "self",
              "href": "http://.../v1/hosts?limit=...&marker=...&..."
            },
            {
              "rel": "next",
              "href": "http://.../v1/hosts?limit=...&marker=...&..."
            }
          ]
        }
    
    Closes-bug: #1658737
    Change-Id: I11c5c6053e5f0873ee53df5b7dcb9b2ed25a0b64

--- craton/api/v1/schemas.py
+++ craton/api/v1/schemas.py
@@ -718,6 +718,38 @@ DefinitionNoParams = {
     "additionalProperties": False,
 }
 
+DefinitionsPaginationLinks = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "rel": {
+                "type": "string",
+                "enum": ["first", "prev", "self", "next"],
+                "description": ("Relation of the associated URL to the current"
+                                " page"),
+            },
+            "href": {
+                "type": "string",
+            },
+        },
+    },
+}
+
+
+def paginated_resource(list_name, schema):
+    return {
+        "type": "object",
+        "additionalProperties": False,
+        "properties": {
+            list_name: {
+                "type": "array",
+                "items": schema,
+            },
+            "links": DefinitionsPaginationLinks,
+        },
+    }
+
 validators = {
     ("ansible_inventory", "GET"): {
         "args": {
@@ -1461,10 +1493,7 @@ filters = {
     ("hosts", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionsHost,
-                "type": "array",
-            },
+            "schema": paginated_resource("hosts", DefinitionsHost),
         },
         400: {
             "headers": None,
@@ -1550,10 +1579,7 @@ filters = {
     ("cells", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionsCell,
-                "type": "array",
-            },
+            "schema": paginated_resource("cells", DefinitionsCell),
         },
         400: {
             "headers": None,
@@ -1585,10 +1611,7 @@ filters = {
     ("regions", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionsRegion,
-                "type": "array",
-            },
+            "schema": paginated_resource("regions", DefinitionsRegion),
         },
         400: {
             "headers": None,
@@ -1660,10 +1683,7 @@ filters = {
     ("projects", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionProject,
-                "type": "array",
-            },
+            "schema": paginated_resource("projects", DefinitionProject),
         },
         400: {
             "headers": None,
@@ -1699,10 +1719,7 @@ filters = {
     ("users", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionUser,
-                "type": "array",
-            },
+            "schema": paginated_resource("users", DefinitionUser),
         },
         400: {
             "headers": None,
@@ -1810,10 +1827,8 @@ filters = {
     ("network_devices", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionNetworkDeviceId,
-                "type": "array",
-            },
+            "schema": paginated_resource("network_devices",
+                                         DefinitionNetworkDeviceId),
         },
         400: {
             "headers": None,
@@ -1957,10 +1972,7 @@ filters = {
     ("networks", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionNetwork,
-                "type": "array",
-            },
+            "schema": paginated_resource("networks", DefinitionNetwork),
         },
         400: {
             "headers": None,
@@ -2050,10 +2062,8 @@ filters = {
     ("network_interfaces", "GET"): {
         200: {
             "headers": None,
-            "schema": {
-                "items": DefinitionNetworkInterface,
-                "type": "array",
-            },
+            "schema": paginated_resource("network_interfaces",
+                                         DefinitionNetworkInterface),
         },
         400: {
             "headers": None,
--- craton/tests/functional/test_host_calls.py
+++ craton/tests/functional/test_host_calls.py
@@ -1,21 +1,51 @@
+import urllib.parse
+
+from craton.tests.functional import TestCase
 from craton.tests.functional.test_variable_calls import \
     APIV1ResourceWithVariablesTestCase
 
 
-class APIV1HostTest(APIV1ResourceWithVariablesTestCase):
+class APIV1HostVariablesTest(APIV1ResourceWithVariablesTestCase):
 
     resource = 'hosts'
 
+    def test_create_host_supports_vars_ops(self):
+        host = self.create_host('host1', 'server', '192.168.1.1')
+        self.assert_vars_get_expected(host['id'], {})
+        self.assert_vars_can_be_set(host['id'])
+        self.assert_vars_can_be_deleted(host['id'])
+
+    def test_host_get_by_vars_filter(self):
+        vars1 = {"a": "b", "host": "one"}
+        self.create_host('host1', 'server', '192.168.1.1', variables=vars1)
+        vars2 = {"a": "b"}
+        self.create_host('host2', 'server', '192.168.1.2', variables=vars2)
+
+        url = self.url + '/v1/hosts?vars=a:b'
+        resp = self.get(url)
+        self.assertEqual(200, resp.status_code)
+        hosts = resp.json()['hosts']
+        self.assertEqual(2, len(hosts))
+        self.assertEqual({'192.168.1.1', '192.168.1.2'},
+                         {host['ip_address'] for host in hosts})
+
+        url = self.url + '/v1/hosts?vars=host:one'
+        resp = self.get(url)
+        self.assertEqual(200, resp.status_code)
+        hosts = resp.json()['hosts']
+        self.assertEqual(1, len(hosts))
+        self.assertEqual('192.168.1.1', hosts[0]['ip_address'])
+        self.assert_vars_get_expected(hosts[0]['id'], vars1)
+
+
+class HostTests(TestCase):
     def setUp(self):
-        super(APIV1HostTest, self).setUp()
+        super(HostTests, self).setUp()
         self.region = self.create_region()
 
-    def tearDown(self):
-        super(APIV1HostTest, self).tearDown()
-
-    def create_region(self):
+    def create_region(self, region_name='region-1'):
         url = self.url + '/v1/regions'
-        payload = {'name': 'region-1'}
+        payload = {'name': region_name}
         region = self.post(url, data=payload)
         self.assertEqual(201, region.status_code)
         self.assertIn('Location', region.headers)
@@ -25,12 +55,16 @@ class APIV1HostTest(APIV1ResourceWithVariablesTestCase):
         )
         return region.json()
 
-    def create_host(self, name, hosttype, ip_address, variables=None):
+    def create_host(self, name, hosttype, ip_address, region=None,
+                    **variables):
+        if region is None:
+            region = self.region
+
         url = self.url + '/v1/hosts'
         payload = {'name': name, 'device_type': hosttype,
                    'ip_address': ip_address,
-                   'region_id': self.region['id']}
-        if variables is not None:
+                   'region_id': region['id']}
+        if variables:
             payload['variables'] = variables
 
         host = self.post(url, data=payload)
@@ -42,15 +76,12 @@ class APIV1HostTest(APIV1ResourceWithVariablesTestCase):
         )
         return host.json()
 
+
+class APIV1HostTest(HostTests):
+
     def test_create_host(self):
         host = self.create_host('host1', 'server', '192.168.1.1')
         self.assertEqual('host1', host['name'])
-
-    def test_create_host_supports_vars_ops(self):
-        host = self.create_host('host1', 'server', '192.168.1.1')
-        self.assert_vars_get_expected(host['id'], {})
-        self.assert_vars_can_be_set(host['id'])
-        self.assert_vars_can_be_deleted(host['id'])
 
     def test_create_with_missing_name_fails(self):
         url = self.url + '/v1/hosts'
@@ -73,51 +104,22 @@ class APIV1HostTest(APIV1ResourceWithVariablesTestCase):
         host = self.post(url, data=payload)
         self.assertEqual(400, host.status_code)
 
-    def test_host_get_all_for_region(self):
-        self.create_host('host1', 'server', '192.168.1.1')
-        self.create_host('host2', 'server', '192.168.1.2')
-        url = self.url + '/v1/hosts?region_id={}'.format(self.region['id'])
-        resp = self.get(url)
-        self.assertEqual(2, len(resp.json()))
-
     def test_host_get_by_ip_filter(self):
         self.create_host('host1', 'server', '192.168.1.1')
         self.create_host('host2', 'server', '192.168.1.2')
         url = self.url + '/v1/hosts?ip_address=192.168.1.1'
         resp = self.get(url)
         self.assertEqual(200, resp.status_code)
-        hosts = resp.json()
+        hosts = resp.json()['hosts']
         self.assertEqual(1, len(hosts))
         self.assertEqual('192.168.1.1', hosts[0]['ip_address'])
-
-    def test_host_get_by_vars_filter(self):
-        vars1 = {"a": "b", "host": "one"}
-        self.create_host('host1', 'server', '192.168.1.1', variables=vars1)
-        vars2 = {"a": "b"}
-        self.create_host('host2', 'server', '192.168.1.2', variables=vars2)
-
-        url = self.url + '/v1/hosts?vars=a:b'
-        resp = self.get(url)
-        self.assertEqual(200, resp.status_code)
-        hosts = resp.json()
-        self.assertEqual(2, len(hosts))
-        self.assertEqual({'192.168.1.1', '192.168.1.2'},
-                         {host['ip_address'] for host in hosts})
-
-        url = self.url + '/v1/hosts?vars=host:one'
-        resp = self.get(url)
-        self.assertEqual(200, resp.status_code)
-        hosts = resp.json()
-        self.assertEqual(1, len(hosts))
-        self.assertEqual('192.168.1.1', hosts[0]['ip_address'])
-        self.assert_vars_get_expected(hosts[0]['id'], vars1)
 
     def test_host_by_missing_filter(self):
         self.create_host('host1', 'server', '192.168.1.1')
         url = self.url + '/v1/hosts?ip_address=192.168.1.2'
         resp = self.get(url)
         self.assertEqual(200, resp.status_code)
-        self.assertEqual(0, len(resp.json()))
+        self.assertEqual(0, len(resp.json()['hosts']))
 
     def test_host_delete(self):
         host = self.create_host('host1', 'server', '192.168.1.1')
@@ -129,3 +131,68 @@ class APIV1HostTest(APIV1ResourceWithVariablesTestCase):
         self.assertEqual(404, resp.status_code)
         self.assertEqual({'status': 404, 'message': 'Not Found'},
                          resp.json())
+
+
+class TestPagination(HostTests):
+
+    def setUp(self):
+        super(TestPagination, self).setUp()
+        self.hosts = [
+            self.create_host('host{}'.format(i), 'server',
+                             '192.168.1.{}'.format(i + 1))
+            for i in range(0, 61)
+        ]
+
+    def test_get_returns_a_default_list_of_thirty_hosts(self):
+        url = self.url + '/v1/hosts'
+        response = self.get(url)
+        self.assertSuccessOk(response)
+        hosts = response.json()
+        self.assertIn('hosts', hosts)
+        self.assertEqual(30, len(hosts['hosts']))
+        self.assertListEqual([h['id'] for h in self.hosts[:30]],
+                             [h['id'] for h in hosts['hosts']])
+
+    def test_get_returns_correct_next_link(self):
+        url = self.url + '/v1/hosts'
+        thirtieth_host = self.hosts[29]
+        response = self.get(url)
+        self.assertSuccessOk(response)
+        hosts = response.json()
+        self.assertIn('links', hosts)
+        for link_rel in hosts['links']:
+            if link_rel['rel'] == 'next':
+                break
+        else:
+            self.fail("No 'next' link was returned in response")
+
+        parsed_next = urllib.parse.urlparse(link_rel['href'])
+        self.assertIn('marker={}'.format(thirtieth_host['id']),
+                      parsed_next.query)
+
+    def test_get_returns_correct_prev_link(self):
+        first_host = self.hosts[0]
+        thirtieth_host = self.hosts[29]
+        url = self.url + '/v1/hosts?marker={}'.format(thirtieth_host['id'])
+        response = self.get(url)
+        self.assertSuccessOk(response)
+        hosts = response.json()
+        self.assertIn('links', hosts)
+        for link_rel in hosts['links']:
+            if link_rel['rel'] == 'prev':
+                break
+        else:
+            self.fail("No 'prev' link was returned in response")
+
+        parsed_prev = urllib.parse.urlparse(link_rel['href'])
+        self.assertIn('marker={}'.format(first_host['id']), parsed_prev.query)
+
+    def test_get_all_for_region(self):
+        region = self.create_region('region-2')
+        self.create_host('host1', 'server', '192.168.1.1', region=region)
+        self.create_host('host2', 'server', '192.168.1.2', region=region)
+        url = self.url + '/v1/hosts?region_id={}'.format(region['id'])
+        resp = self.get(url)
+        self.assertSuccessOk(resp)
+        hosts = resp.json()
+        self.assertEqual(2, len(hosts['hosts']))
