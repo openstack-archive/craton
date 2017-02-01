7fe72c678c81f1e42bdebb37f92f929ea92439af
Thomas Maddox <thomas.maddox@rackspace.com>
2017-02-01 15:34:33 +0000

    Adds Variables support to Projects resource
    Includes Variables support, which is already present
    in several other Craton resources, in the Projects
    resource as well.
    
    Project variables will serve as a top-most scope
    of variables for resources that belong to a specific
    project.
    
    Closes Bug: #1648626
    
    Change-Id: I574e834ce409efd8ceafb15fe30a86ae93df2a80

--- craton/api/v1/schemas.py
+++ craton/api/v1/schemas.py
@@ -344,6 +344,7 @@ DefinitionProject = {
         "name": {
             "type": "string",
         },
+        "variables": DefinitionVariablesSource,
     },
 }
 
@@ -961,6 +962,14 @@ validators = {
     ("projects_id", "DELETE"): {
     },
     ("projects_id", "GET"): {
+    },
+    ("projects_id_variables", "PUT"): {
+        "json": DefinitionVariablesSource,
+    },
+    ("projects_id_variables", "GET"): {
+    },
+    ("projects_id_variables", "DELETE"): {
+        "json": DefinitionVariablesSource,
     },
     ("users", "GET"): {
         "args": {
@@ -1795,6 +1804,70 @@ filters = {
         },
     },
     ("regions_id", "DELETE"): {
+        204: {
+            "headers": None,
+            "schema": None,
+        },
+        400: {
+            "headers": None,
+            "schema": None,
+        },
+        404: {
+            "headers": None,
+            "schema": None,
+        },
+        405: {
+            "headers": None,
+            "schema": None,
+        },
+    },
+    ("projects_id_variables", "GET"): {
+        200: {
+            "headers": None,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "variables": DefinitionVariablesSource,
+                },
+            },
+        },
+        400: {
+            "headers": None,
+            "schema": None,
+        },
+        404: {
+            "headers": None,
+            "schema": None,
+        },
+        405: {
+            "headers": None,
+            "schema": None,
+        },
+    },
+    ("projects_id_variables", "PUT"): {
+        200: {
+            "headers": None,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "variables": DefinitionVariablesSource,
+                },
+            },
+        },
+        400: {
+            "headers": None,
+            "schema": None,
+        },
+        404: {
+            "headers": None,
+            "schema": None,
+        },
+        405: {
+            "headers": None,
+            "schema": None,
+        },
+    },
+    ("projects_id_variables", "DELETE"): {
         204: {
             "headers": None,
             "schema": None,
