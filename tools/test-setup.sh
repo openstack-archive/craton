a7772dbc74511493c7da01cb98eb30c84a286a7f
Ian Cordasco <graffatcolmingov@gmail.com>
2017-02-16 11:31:36 -0600

    Fix up functional testing
    OpenStack's incredibly helpful infra team pointed out that this was
    something we can do to fix our functional tests and should make it far
    faster to clean up our gate. Let's see how it goes.
    
    Change-Id: I732d144e4635dffe8034c0d374eee591df078f52

---
+++ tools/test-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script will execute before OpenStack infrastructure revokes sudo
+# We need it to allow the Jenkins user to interact with docker
+
+echo "=> Add user to docker group"
+sudo gpasswd -a ${USER} docker
+echo "=> Restarting docker"
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+echo "=> User permissions: $(id)"
+echo "=> Launching a new shell"
+exec su -l ${USER}
+echo "=> User permissions: $(id)"
+echo "=> Discovering docker version installed"
+docker version
