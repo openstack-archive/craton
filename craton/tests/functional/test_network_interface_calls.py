bb4304eeeeb4156c77e6065e31fa95815e38018a
Ian Cordasco <graffatcolmingov@gmail.com>
2017-03-01 14:15:07 -0600

    Update schema definition for network interfaces port
    Previously, listing or creating a network interface required the port be
    an integer. Updating the port, however, allowed it to be a string
    instead of similarly requiring it to be an integer. This updates the
    constraint and adds the basis of functional testing for those endpoints.
    
    Closes-bug: #1630276
    Change-Id: Ic99d73dfd63fb3fe9a64d41ca2b8eafe4ff1c253

---
+++ craton/tests/functional/test_network_interface_calls.py
@@ -0,0 +1,57 @@
+from craton.tests import functional
+
+
+class APIv1NetworkInterfacesTest(functional.DeviceTestBase):
+    def setUp(self):
+        super(APIv1NetworkInterfacesTest, self).setUp()
+        self.interfaces_url = self.url + '/v1/network-interfaces'
+
+    def test_associate_network_device_with_a_host(self):
+        host = self.create_host('host-0', 'server', '127.0.0.1')
+
+        payload = {
+            'name': 'lo',
+            'ip_address': '127.0.0.1',
+            'device_id': host['id'],
+            'interface_type': 'loopback',
+        }
+        response = self.post(self.interfaces_url, data=payload)
+        self.assertSuccessCreated(response)
+        self.assertIn('Location', response.headers)
+        interface = response.json()
+        self.assertEqual(
+            '{}/{}'.format(self.interfaces_url, interface['id']),
+            response.headers['Location']
+        )
+
+    def test_port_must_be_an_integer_on_create(self):
+        host = self.create_host('host-0', 'server', '127.0.0.1')
+
+        payload = {
+            'name': 'lo',
+            'ip_address': '127.0.0.1',
+            'device_id': host['id'],
+            'interface_type': 'loopback',
+            'port': 'asdf',
+        }
+        response = self.post(self.interfaces_url, data=payload)
+        self.assertBadRequest(response)
+
+    def test_port_must_be_an_integer_on_update(self):
+        host = self.create_host('host-0', 'server', '127.0.0.1')
+
+        payload = {
+            'name': 'lo',
+            'ip_address': '127.0.0.1',
+            'device_id': host['id'],
+            'interface_type': 'loopback',
+            'port': 80,
+        }
+        response = self.post(self.interfaces_url, data=payload)
+        self.assertSuccessCreated(response)
+        interface = response.json()
+
+        url = self.interfaces_url + '/{}'.format(interface['id'])
+        payload = {'port': 'asdf'}
+        response = self.put(url, data=payload)
+        self.assertBadRequest(response)
