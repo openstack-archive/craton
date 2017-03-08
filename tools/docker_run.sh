7e7528e3dea5dde1b9a376018f36fc4460a82630
Sulochan Acharya <sulochan@gmail.com>
2017-03-08 15:18:14 +0000

    Adds project/user bootstrap command to dbsync
    So far we have been using external mysql command
    to insert project/user to bootstrap the process.
    This caused much frustration since operators needed
    to insert records in the db. This patch adds commands
    to dbsync to allow creating project/users. This method
    also takes care of association_id.
    
    To initiate a bootstrap:
    craton-dbsync --config-file=craton.conf bootstrap
    
    which will create a project with a root user.
    
    Users using Dockerfile can look at docker logs
    after creating the container to find this info.
    
    Closes Bug: 1670561
    
    Change-Id: I9372961ca6623d530d7844b9f38aade544d961e8

--- tools/docker_run.sh
+++ tools/docker_run.sh
@@ -19,31 +19,14 @@ mysqladmin flush-privileges
 ##############
 /craton/bin/craton-dbsync --config-file=/craton/etc/craton-api-conf.sample upgrade
 
-PROJECT="b9f10eca66ac4c279c139d01e65f96b4"
-
-BOOTSTRAP_USERNAME="bootstrap"
-BOOTSTRAP_TOKEN="bootstrap"
-
-USERNAME="demo"
-TOKEN="demo"
-
-ADMIN_USERNAME="demo_admin"
-ADMIN_TOKEN="demo_admin"
-
-ROOT_USERNAME="demo_root"
-ROOT_TOKEN="demo_root"
-
-PROJECT_DISCRIMINATOR='project'
 
 ####################################
 # Create initial project and users #
 ####################################
-PROJECT_VA_ID=$(mysql -u root craton -e "INSERT into variable_association (created_at, updated_at, discriminator) values (NOW(), NOW(), '$PROJECT_DISCRIMINATOR'); SELECT LAST_INSERT_ID();" | grep -Eo '[0-9]+')
-mysql -u root craton -e "INSERT into projects (created_at, updated_at, name, variable_association_id, id) values (NOW(), NOW(), '$USERNAME', $PROJECT_VA_ID, '$PROJECT')"
-mysql -u root craton -e "INSERT into users (created_at, updated_at, project_id, username, api_key, is_root, is_admin) values (NOW(), NOW(), '$PROJECT', '$BOOTSTRAP_USERNAME', '$BOOTSTRAP_TOKEN', True, False)"
-mysql -u root craton -e "INSERT into users (created_at, updated_at, project_id, username, api_key, is_root, is_admin) values (NOW(), NOW(), '$PROJECT', '$USERNAME', '$TOKEN', False, False)"
-mysql -u root craton -e "INSERT into users (created_at, updated_at, project_id, username, api_key, is_root, is_admin) values (NOW(), NOW(), '$PROJECT', '$ADMIN_USERNAME', '$ADMIN_TOKEN', False, True)"
-mysql -u root craton -e "INSERT into users (created_at, updated_at, project_id, username, api_key, is_root, is_admin) values (NOW(), NOW(), '$PROJECT', '$ROOT_USERNAME', '$ROOT_TOKEN', True, True)"
+# NOTE(sulo): One initial bootstrap project with root user will be created by the
+# bootstrap process. Users can docker logs -f <container-id> to view their api-key
+# to use with the client.
+/craton/bin/craton-dbsync --config-file=etc/craton-api-conf.sample bootstrap
 
 #########################
 # Start the API service #
