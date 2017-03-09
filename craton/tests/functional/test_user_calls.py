7c1c8b3d4c6cc2220533d1e7b6e65d015e659589
Thomas Maddox <thomas.maddox@rackspace.com>
2017-03-09 17:11:45 +0000

    JSON Path-like querying for variables
    This patch implements the ability to query for values
    in nested variable documents. For instance, if we had
    hosts with a variables that look like:
    
    {
        "hardware_profiles": {
            "disks": [
                {
                    "manufacturer": "seagate",
                    "capacity_quantity": 2,
                    "capacity_unit": "TB"
                },
                {
                    "manufacturer": "western",
                    "capacity_quantity": 3,
                    "capacity_unit": "TB"
                }
            ]
        }
    }
    
    and we wanted to grab all of them with a disk that was
    manufactured by Seagate, we would like to be able to
    query like so:
    
    GET /v1/hosts?vars=hardware_profiles.disks[*].manufacturer:seagate
    
    Change-Id: Id597d3e57d2e28766fecd1b314f53176543e1b9d
    Closes-Bug: 1671116

--- craton/tests/functional/test_user_calls.py
+++ craton/tests/functional/test_user_calls.py
@@ -4,19 +4,9 @@ from craton.tests import functional
 
 
 class UserTests(functional.TestCase):
-    def setUp(self):
-        super(UserTests, self).setUp()
-        self.root_headers = copy.deepcopy(self.session.headers)
-        self.root_headers[functional.HEADER_USERNAME] = \
-            functional.FAKE_DATA_GEN_BOOTSTRAP_USERNAME
-        self.root_headers[functional.HEADER_TOKEN] = \
-            functional.FAKE_DATA_GEN_BOOTSTRAP_TOKEN
-
-    def tearDown(self):
-        super(UserTests, self).tearDown()
 
     def test_create_user(self):
-        project = self.create_project('test', headers=self.root_headers)
+        project = self.create_project('test')
         url = self.url + '/v1/users'
         payload = {'username': 'testuser', 'project_id': project['id']}
         user = self.post(url, data=payload)
@@ -25,7 +15,7 @@ class UserTests(functional.TestCase):
         self.assertEqual(payload['project_id'], user.json()['project_id'])
 
     def test_create_user_with_admin_priv(self):
-        project = self.create_project('test', headers=self.root_headers)
+        project = self.create_project('test')
         url = self.url + '/v1/users'
         payload = {'username': 'testuser', 'project_id': project['id'],
                    'is_admin': True}
